81c118d68e3bd16a461c7b9ef97b4f3fc8218090
illya06 <47461573+illya06@users.noreply.github.com>
2020-12-29 09:16:47 +0200

    added Task16 class

---
+++ ASD_LAB_12/Task16.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task16__
+{
+    /*
+    
+        TO DO : 
+        
+        16.   
+        Задано два тексти.
+        В першому тексті знайти слово 
+        яке складається з найменшої кількості голосних літер 
+        знайти його входження в другий текст відповідним алгоритмом пошуку.
+    
+    */
+
+    class Task16
+    {
+        public static string perform(string text)
+        {
+            var words = text.Split(' ', ',', '.', ':', '\t').Distinct();
+            int max = 0;
+            string ret = "";
+            foreach(string word in words)
+            {
+                int temp = 0;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+                    {
+                        temp++;
+                    }
+                }
+                if(temp > max)
+                {
+                    max = temp;
+                    ret = word;
+                }
+            }
+            return ret;
+            
+        }
+    }
+}
